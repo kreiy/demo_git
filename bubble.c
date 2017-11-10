35e7b2c91c50e9c182b0144189fab67cd346112c
Yasmina <kezimanareineyasmina@yahoo.fr>
2017-11-10 14:52:48 +0400

    code du trie a bubble

---
+++ bubble.c
@@ -0,0 +1,68 @@
+
+#include<iostream>
+using namespace std;
+
+
+
+
+void Bubble_sort( int t[],int l)
+{
+
+	int i,j,temp;
+	
+    for(i = 0;i < l-1;i++)
+	{
+	
+	   
+	    cout<<"pass"<< i<< endl;
+		for(j = 0;j < l;j++)
+		{
+		
+		    if(t[j] > t[j+1])
+			{
+				
+				temp = t[j];
+				t[j] = t[j+1];
+				t[j+1] = temp;
+				
+			}
+			
+			cout<<"\t"<< t[j]<< endl;
+			
+		}
+	}
+	
+}
+
+
+
+void sort_array(int taille)
+{
+
+   int i,chiffre;
+   int tableau[taille];
+   
+   for(i = 0; i < taille; i++)
+   {
+   	    cout<<"\nVeuillez inserer le nombre:";
+	    cin>> chiffre;
+	    tableau[i] = chiffre;
+   }
+   
+ 
+  Bubble_sort(tableau,taille);
+  
+}
+
+
+
+int main()
+{
+    int m;
+    cout<<"Veuillez preciser la taille du tableau:"<<endl;
+	cin>> m;
+	
+	sort_array(m);
+	
+	return 0;
+}
